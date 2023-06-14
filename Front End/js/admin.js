b0c63fd67248b853175f2ccdf380a33b138abe8b
MohammedRazee <razeeismail15@gmail.com>
2023-06-14 11:58:32 +0530

    delete old Front End

--- Front End/js/admin.js
+++
@@ -1,8 +0,0 @@
-// const Displayer = document.querySelector('.category-edit');
-
-// Displayer.forEach((edit, index) => {
-//     edit.addEventListener('click', ()=> {
-//         // document.querySelector(".signup_container").style.display = 'block';
-//         document.querySelector('.table').style.display = 'none';
-//     });
-// });
